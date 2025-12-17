f7647188e2833f853ce8923fe63adecda68f52a2
LuizCorrei4 <167918051+LuizCorrei4@users.noreply.github.com>
2025-12-17 22:12:14 +0000

    testando algumas funções de texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 
-st.title("Site teste da Escola de Programação")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Site teste da Escola de Programação - By Luiz Gabriel")
+
+# titulo
+st.header("Bem-vindo!")
+
+st.subheader("1ª Seção")
+st.write("Conteúdo da 1ª seção")
+
+st.subheader("2ª Seção")
+st.write("Conteúdo da 2ª seção")
+
+# textos em markdown
+st.markdown("""---""")
+st.markdown("# Titulo em markdown")
+st.markdown("Conteúdo em **markdown**")
+
+st.markdown("""
+            Lista de tarefas:
+            1. Primeiro passo
+            2. Segundo passo
+            """)
+
+# usando html 
+st.markdown("### HTML")
+
+html_code = """
+    <h1 style = 'color: blue;'> Esse é um cabeçalho azul </h1>
+    <p style = 'color: purple;'> Esse é um parágrafo </p>
+"""
+
+st.markdown(html_code, unsafe_allow_html= True)
