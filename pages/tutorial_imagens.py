e6dfbffb65bd117dd091137a44ebc0af479767d2
LuizCorrei4 <167918051+LuizCorrei4@users.noreply.github.com>
2026-01-14 23:00:18 +0000

    algumas alterações

--- pages/tutorial_imagens.py
+++ pages/tutorial_imagens.py
@@ -1,3 +1,14 @@
 import streamlit as st 
 
 st.title("Tutorial de Imagens")
+st.image("img/img_3_meninas.jpeg")
+
+# colocando imagens na mesma linha
+colunas = st.columns(3)
+
+colunas[0] = st.image("img/like_insta.jpeg")
+colunas[1] = st.image("img/like_insta.jpeg")
+colunas[2] = st.image("img/like_insta.jpeg")
+
+st.file_uploader("Escolha uma imagem", type = ["jpg", "jpeg", "gif", "png"])
+
